3875b5acfd82debc30f334b2747cc10f808ee158
dohan.kim <junyeob.kim@onch3.co.kr>
2026-04-09 15:27:42 +0900

    style: modi

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom' // createHashRouter로 변경
 import App from './App.tsx'
 import FirebaseTest from './pages/FirebaseTest.tsx'
 import './index.css'
 
-// 라우터 설정 생성
-const router = createBrowserRouter([
+// createBrowserRouter 대신 createHashRouter 사용
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
   },
   {
-    // 이제 /dailypharm-survey/test 로 접속하면 이 컴포넌트가 뜹니다.
-    path: "/test", 
+    path: "/test",
     element: <FirebaseTest />,
   },
-], {
-  // 프로젝트가 실행되는 기본 경로(Base URL)를 지정합니다.
-  basename: "/dailypharm-survey", 
-});
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
