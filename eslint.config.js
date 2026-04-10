2e1f72bdb26663c2eaab3f4586ad93c46ac0663b
dohan.kim <junyeob.kim@onch3.co.kr>
2026-04-10 18:24:54 +0900

    빌드 옵션 변경

--- eslint.config.js
+++ eslint.config.js
@@ -11,28 +11,13 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended, // 배열 전개 연산자 확인
+      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      // reactRefresh.configs.vite, // 플러그인에 따라 설정 방식이 다를 수 있음
+      reactRefresh.configs.vite,
     ],
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    // 핵심: 여기에 규칙을 추가합니다.
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      // 사용하지 않는 변수가 있어도 에러(2)가 아닌 경고(1)만 표시하거나 끔(0)
-      'no-unused-vars': 'off', 
-      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ])
